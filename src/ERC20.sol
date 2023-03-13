1eca1d5a0fa28fbc3832088ffd623b824b36b02b
kimziwu <jiwoo.kim@dream-academy.io>
2023-03-13 21:41:00 +0900

    test

---
+++ src/ERC20.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier : MIT
+pragma solidity ^0.8.0;
+
+
+
+contract ERC20 {
+    mapping(address=>uint256) private balances;
+    mapping(address=>mapping(address=>uint256)) private allowances;
+
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    constructor () {
+        _name="DREAM";
+        _symbol="DRM";
+        _decimals=18;
+        _totalSupply=100 ether;
+        balances[msg.sender] = 100 ether;
+    }
+
+    function name() public view returns (string memory){
+        return _name;
+    }
+
+    function symbol() public view returns (string memory){
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint256){
+        return _decimals;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256){
+        return balances[_owner];
+    }
+
+
+    function transfer(address _to, uint256 _value) external returns (bool success){
+        require(_to!=address(0), "transfer to the zero address");
+        require(balances[msg.sender]>=_value, "value exceeds balance");
+
+        unchecked {
+            balances[msg.sender]-=_value;
+            // - : underflow -> revert
+            balances[_to]+=_value;
+        }
+
+        emit Transfer(msg.sender,_to,_value);
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        require(_spender!=address(0),"approve to the zero address");
+
+        allowances[msg.sender][_spender]=_value;
+        emit Approval(msg.sender,_spender,_value);
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining){
+        return allowances[_owner][_spender];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success){
+        require(_from!=address(0), "from : zero addr");
+        require(_to!=address(0), "to : zero addr");
+
+        uint256 currentAllowance=allowance(_from, msg.sender);
+        if (currentAllowance!=type(uint256).max){
+            require(currentAllowance>=_value, "value exceeds balance");
+            unchecked {
+                allowances[_from][msg.sender]-=_value; //안하면 계속 보내지는 문제 발생
+            }
+        }
+        
+        require(balances[_from]>=_value,"value exceeds balance");
+
+        unchecked {
+            balances[_from]-=_value;
+            balances[_to]+=_value;
+        }
+        emit Transfer(_from,_to,_value);
+    }
+    
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+    function _mint(address _owner, uint256 _value) internal {
+        require(_owner!=address(0),"mint to the zero address");
+        _totalSupply+=_value;
+        unchecked {
+            balances[_owner]+=_value;
+        }
+        emit Transfer(address(0),_owner,_value);
+    }
+
+    function _burn(address _owner, uint256 _value) internal {
+        require(_owner!=address(0),"burn from the zero address");
+        require(balances[_owner]>=_value,"burn amount exeeds balance");
+        unchecked {
+            balances[_owner]-=_value;
+            _totalSupply-=_value;
+        }
+        emit Transfer(_owner,address(0), _value);
+    }
+    
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+}
+
